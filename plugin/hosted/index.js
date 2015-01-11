593aeeaf885fa5cd4c586473a23cca600f4743e0
Katie Miller <katie@codemiller.com>
2015-01-12 12:14:28 +1300

    Adding env var

--- plugin/hosted/index.js
+++ plugin/hosted/index.js
@@ -44,7 +44,7 @@ var opts = {
 	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ipAddr : process.env.IP_ADDR || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   web_host: process.env.REVEAL_WEB_HOST || process.env.OPENSHIFT_APP_DNS || 'localhost:8080',
-  socket_host: process.env.REVEAL_SOCKET_HOST || process.env.REVEAL_WEB_HOST || process.env.OPENSHIFT_APP_DNS || 'localhost',
+  socket_host: process.env.REVEAL_SOCKET_HOST || process.env.REVEAL_WEB_HOST || process.env.OPENSHIFT_APP_DNS || process.env.GIST_REVEAL_SERVICE_SERVICE_HOST || 'localhost',
   socket_secret : process.env.REVEAL_SOCKET_SECRET,
   default_gist_id : process.env.DEFAULT_GIST || 'af84d40e58c5c2a908dd',
   theme : process.env.REVEAL_THEME || '60e54843de11a545897e',
